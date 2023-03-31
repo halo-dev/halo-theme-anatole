50dcdf8f43b35e2c12dc4e82be4b15908022e8c2
Ryan Wang <i@ryanc.cc>
2023-03-31 15:43:44 +0800

    feat: add supports for plugin-moments

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,17 @@
 module.exports = {
-    root: true,
-    parser: "@typescript-eslint/parser",
-    plugins: ["@typescript-eslint", "prettier"],
-    extends: [
-      "eslint:recommended",
-      "plugin:@typescript-eslint/eslint-recommended",
-      "plugin:@typescript-eslint/recommended",
-      "prettier",
-    ],
-    env: {
-      node: true,
-    },
-  };
-  
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "prettier"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  env: {
+    node: true,
+  },
+  rules: {
+    "@typescript-eslint/ban-ts-comment": "off",
+  },
+};
